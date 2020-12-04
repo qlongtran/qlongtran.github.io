938ad6da560987a7a9b5455fa3bf5b041020a76e
qlongtran <70735365+qlongtran@users.noreply.github.com>
2020-12-04 10:38:52 -0800

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,86 @@
+function insta() {
+    window.open("https://www.instagram.com/d1darian")
+}
+function soundcloud() {
+    window.open("https://soundcloud.com/d1darian")
+}
+function spotify() {
+    window.open("https://open.spotify.com/artist/18K72qSHBxT9W3ub7uy2kG")
+}
+function apple() {
+    window.open("https://music.apple.com/us/artist/d1darian/1488602432")
+}
+$(function() {
+    $("#home").click(function() {
+      $("#rapper").toggleClass("darkmode");
+    });
+  });
+$(function() {
+    $("#home").click(function() {
+      $("#fame").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("#info").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".breakbar").toggleClass("darkmode");
+    });
+  });
+  $(function() {
+    $("#home").click(function() {
+      $("span").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("body").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("#home,#about,#contact").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("#homeimg").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".line1").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".hline4").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".hline5").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".line2").toggleClass("darkmode");
+    });
+  });
+  $(function() {
+    $("#home").click(function() {
+      $("#porcheimg").toggleClass("darkmode");
+    });
+  });
