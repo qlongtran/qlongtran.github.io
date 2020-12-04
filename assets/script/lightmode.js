3745242c518df8c6a66e47d9fc1d3ed7a18de1a4
qlongtran <70735365+qlongtran@users.noreply.github.com>
2020-12-04 10:49:57 -0800

    update files

---
+++ assets/script/lightmode.js
@@ -0,0 +1,74 @@
+$(function() {
+    $("#home").click(function() {
+      $("#rapper").toggleClass("darkmode");
+    });
+  });
+$(function() {
+    $("#home").click(function() {
+      $("#fame").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("#info").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".breakbar").toggleClass("darkmode");
+    });
+  });
+  $(function() {
+    $("#home").click(function() {
+      $("span").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("body").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("#home,#about,#contact").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $("#homeimg").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".line1").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".hline4").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".hline5").toggleClass("darkmode");
+    });
+  });
+
+  $(function() {
+    $("#home").click(function() {
+      $(".line2").toggleClass("darkmode");
+    });
+  });
+  $(function() {
+    $("#home").click(function() {
+      $("#porcheimg").toggleClass("darkmode");
+    });
+  });
